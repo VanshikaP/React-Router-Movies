bbac7f85ea258d6f9ffeee7e18d2c7b857ddc209
VanshikaP <vanshikapundir92@gmail.com>
2020-02-24 12:16:12 -0800

    Refined Stying

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -36,7 +36,7 @@ const Movie = (props) => {
   return (
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>Movie file {title}</h2>
+        <h2>{title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
         </div>
