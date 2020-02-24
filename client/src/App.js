1c8d86c69ae38a5b85be111ff0e6f8091fa5b622
VanshikaP <vanshikapundir92@gmail.com>
2020-02-24 12:04:45 -0800

    Added Save Movie Functionality

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ const App = () => {
       <SavedList list={savedList} />
       <Route exact path='/' component={MovieList} />
       <Route exact path='/movies/:movieID'>
-        <Movie />
+        <Movie addToSavedList={addToSavedList}/>
       </Route>
 
     </div>
--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -19,14 +19,14 @@ const Movie = (props) => {
         .catch(error => {
           console.error(error);
         });
-
-  },[]);
+        
+  },[movieID]);
   
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie);
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
@@ -36,7 +36,7 @@ const Movie = (props) => {
   return (
     <div className="save-wrapper">
       <div className="movie-card">
-        <h2>{title}</h2>
+        <h2>Movie file {title}</h2>
         <div className="movie-director">
           Director: <em>{director}</em>
         </div>
@@ -51,7 +51,7 @@ const Movie = (props) => {
           </div>
         ))}
       </div>
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={saveMovie} >Save</div>
     </div>
   );
 }
