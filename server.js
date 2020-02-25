e36f5a25fc982288078198c20a3f1576486334c3
VanshikaP <vanshikapundir92@gmail.com>
2020-02-25 12:40:30 -0800

    Removed index server

--- server.js
+++ server.js
@@ -82,15 +82,3 @@ app.set( 'port', ( process.env.PORT || 5000 ));
 app.listen( app.get( 'port' ), function() {
   console.log( 'Node server is running on port ' + app.get( 'port' ));
   });
-
-//Trial 4 - 
-// if (process.env.NODE_ENV === 'production') {
-// // Exprees will serve up production assets
-// 	app.use(express.static('client/build'));
-
-// 	// Express serve up index.html file if it doesn't recognize route
-// 	const path = require('path');
-// 	app.get('*', (req, res) => {
-// 		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-// 		});
-// }
