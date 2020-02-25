4925cd6f1a2d809fd40eb6de2bad7a3321653d4c
VanshikaP <vanshikapundir92@gmail.com>
2020-02-25 10:45:12 -0800

    Port changes

--- server.js
+++ server.js
@@ -66,6 +66,8 @@ app.post('/api/movies', (req, res) => {
 	res.status(201).json(movies);
 });
 
-app.listen(5000, () => {
-	console.log('Server listening on port 5000');
-});
+// app.listen(5000, () => {
+// 	console.log('Server listening on port 5000');
+// });
+
+app.listen(process.env.PORT || 5000);
